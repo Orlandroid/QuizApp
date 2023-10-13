2de420f96055b5cefd001756045572b95d286251
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-10-12 21:49:03 -0600

    Add HomeView

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -80,5 +80,5 @@ dependencies {
     implementation(Dependencies.GLIDE)
     annotationProcessor(Dependencies.GLIDE_COMPILER)
     implementation(Dependencies.SWIPE_REFRESH_LAYOUT)
-    //implementation("com.github.Spikeysanju:MotionToast:1.4")
+    implementation("com.airbnb.android:lottie:6.0.0")
 }
---
+++ presentation/src/main/java/com/example/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.presentation.ui.home
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+
+    override fun setUpUi() {
+
+    }
+
+
+}
